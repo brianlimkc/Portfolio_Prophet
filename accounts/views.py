71507da351b9656aa069aad440930bc4395e8c4b
chiakaiming <chiakaiming@hotmail.com>
2021-07-13 14:11:44 +0800

    pwd is now hased

--- accounts/views.py
+++ accounts/views.py
@@ -5,15 +5,16 @@ from rest_framework.decorators import api_view, permission_classes
 from django.http.response import JsonResponse
 from accounts.serializers import UserSerializer
 from accounts.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 @api_view(['GET', 'POST'])
 def register_user(request):
     if request.method == "POST":
         new_user = UserSerializer(data=request.data)
-        print(request.data)
+
         if new_user.is_valid():
             print("success")
             new_user.save()
 
-    return Response({"message" : ""}, status=status.HTTP_201_CREATED)
+    return Response({"message" : "user is registered"}, status=status.HTTP_201_CREATED)
