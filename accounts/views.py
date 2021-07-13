7238dd5f358bbd03db3caec47b9ddf6b4b0da3e0
chiakaiming <chiakaiming@hotmail.com>
2021-07-13 13:08:04 +0800

    setup register route and restframework

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 # Create your views here.
+
+@api_view(['GET'])
+def register_user(request):
+    
+
+    return Response({"message" : "route hit"}, status=status.HTTP_201_CREATED)
--- portfolio_prophet/urls.py
+++ portfolio_prophet/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio_test.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('accounts/', include('accounts.urls'))
 ]
