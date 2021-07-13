62b2b503dc8a6336ba9bcf8e9b9d14cb548f25ed
BrianLim80 <brian.lim.kc@gmail.com>
2021-07-13 22:31:13 +0800

    futher tweaking of code

--- portfolio_test/urls.py
+++ portfolio_test/urls.py
@@ -4,5 +4,6 @@ from portfolio_test.views import *
 urlpatterns = [
     path('show', show_stock, name="show_stock"),
     path('show_all', show_all, name="show_all"),
-    path('get_stocks', populate_stock_history, name='populate_stock_history')
+    path('get_stocks', populate_stock_history, name='populate_stock_history'),
+    path('populate', populate_stocksdb, name="populate_stocksdb"),
 ]
