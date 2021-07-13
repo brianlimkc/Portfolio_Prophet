8f0aeb7c734d1a85c5adc4df56fac6909e201462
chiakaiming <chiakaiming@hotmail.com>
2021-07-13 11:25:30 +0800

    merge w faith

--- portfolio_test/views.py
+++ portfolio_test/views.py
@@ -12,37 +12,6 @@ def show_stock(request):
 
     if stock==None:
         stock="GOOG"
-<<<<<<< HEAD
-        
-    START = "2015-01-01"
-    TODAY = date.today().strftime("%Y-%m-%d")
-    stock_name = stock
-
-    ticker = yf.Ticker(stock_name)
-    stock_info = ticker.info
-
-    period = 1 * 365
-
-    stock_history = ticker.history(start=START, end=TODAY)  
-    stock_history.reset_index(inplace=True)
-    results_length = stock_history.shape[0]
-  
-    stock_result = {
-        "name" : stock_info["shortName"],
-        "symbol" : stock_info["symbol"],
-        "quoteType" : stock_info["quoteType"],
-        "currentPrice" : stock_info["currentPrice"],
-        "industry" : stock_info["industry"],
-        "marketCap" : stock_info["marketCap"],
-        "close" : round(stock_history["Close"][results_length-1],2),
-        "high" : round(stock_history["High"][results_length-1],2),
-        "low" : round(stock_history["Low"][results_length-1],2),
-        "volume" : stock_history["Volume"][results_length-1],
-        "price_change" : round(stock_history["Close"][results_length-2] - stock_history["Close"][results_length-1],2),
-        "percent_change" : round((stock_history["Close"][results_length-2] - stock_history["Close"][results_length-1])/100,2)
-    }
-=======
->>>>>>> 5fdd985ed28ff346d02e174287954da22f1c6946
 
     stock_record = Stock.objects.get(symbol=stock)
     
