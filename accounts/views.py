01bf8fa29fd60750691c4be2196623e249a5c2c7
KaiMing Chia <chiakaiming@hotmail.com>
2021-07-14 16:56:49 +0800

    finally??

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from accounts.models import User
 from accounts.forms import RegisterUserForm
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 
 @api_view(['POST'])
+@authentication_classes([])
 @permission_classes([AllowAny])
 def register_user(request):
     if request.method == "POST":
@@ -17,8 +19,12 @@ def register_user(request):
         form = RegisterUserForm(request.data)
 
         if form.is_valid():
-            form.save()
-            return Response({"message" : "user is registered"}, status=status.HTTP_201_CREATED)
+            user = form.save()
+            print("user data",user.id)
+            tokens = RefreshToken.for_user(user)
+            return Response({"refresh" : str(tokens),
+            "access" : str(tokens.access_token)
+            }, status=status.HTTP_201_CREATED)
         else:
             print(form.errors)
             return Response({"message" : "check input for user"}, status=status.HTTP_400_BAD_REQUEST)
