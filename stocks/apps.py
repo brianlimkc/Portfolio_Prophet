3c2f008da1c24c1bfe8eb1724d99a748d8fe0bdb
BrianLim80 <brian.lim.kc@gmail.com>
2021-07-14 22:39:44 +0800

    app rename successful

--- stocks/apps.py
+++ stocks/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PortfolioTestConfig(AppConfig):
+class StocksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portfolio_test'
+    name = 'stocks'
